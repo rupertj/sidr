c0524b36739fb83776bc58ee56a4b8a7e5e1e08e
Rupert Jabelman <rupert.jabelman@torchbox.com>
2015-09-18 11:36:07 +0100

    Refactored DrupalClient so it doesn't extend Guzzle's Client any more. Added start of higher-level API.

--- src/DrupalClient.php
+++ src/DrupalClient.php
@@ -2,30 +2,48 @@
 
 namespace SIDR;
 
+use Silex\Application;
 use GuzzleHttp;
 
 /**
  * Class DrupalClient
  * @package SIDR
  *
- * @see DrupalServiceProvider for how to register this in a Silex app.
+ * To use this in a project, register it like this:
+ *
+ *
+ * $app->register(new SIDR\DrupalServiceProvider(), [
+ *   'drupal.backend' => 'http://example.com',
+ *   'drupal.endpoint' => 'services',
+ * ]);
+ *
  */
 
-class DrupalClient extends GuzzleHttp\Client {
+class DrupalClient {
 
-  /**
-   * @todo: Refactor this class to use composition over inheritance.
-   * We don't actually need it to extend Guzzle's client, instead it'd be nice
-   * to build a higher-level API here.
-   */
+  /** @var  Application */
+  protected $app;
 
-  private $app;
-  private $backend;
-  private $endpoint;
+  /** @var  String */
+  protected $backend;
+
+  /** @var  String */
+  protected $endpoint;
+
+  /** @var Array */
+  protected $terms;
+
+  /** @var GuzzleHttp\Client **/
+  protected $client;
 
   public function __construct($app, array $config = []) {
     $this->app = $app;
-    parent::__construct($config);
+
+    // @todo: Add support for a null cache or other caches. (PSR-6?)
+    $this->cache = $app['memcache'];
+
+    // @todo: DI.
+    $this->client = new GuzzleHttp\Client($config);
   }
 
   public function setBackend($backend, $endpoint) {
@@ -109,13 +127,6 @@ class DrupalClient extends GuzzleHttp\Client {
       return implode('&', $params);
   }
 
-  /**
-   * Method for get requests.
-   * @param null $url
-   * @param array $options
-   * @return mixed
-   */
-
   public function get($url = null, $options = []) {
     return $this->request('get', $url, $options);
   }
@@ -162,12 +173,11 @@ class DrupalClient extends GuzzleHttp\Client {
       $backend = $this->backend . '/';
     }
 
-    // Don't pass this to parent::$method. It's not a real guzzle option and
-    // just confuses guzzle.
+    // Don't pass this to the client
     unset($options['prefix_endpoint']);
 
-    $this->app['timer']->start($url);
-    $response = parent::$method($backend . $url, $options);
+    $this->app['timer']->start($url . ' ' . serialize($options));
+    $response = $this->client->$method($backend . $url, $options);
     $this->app['timer']->end();
 
     return $response;
@@ -176,5 +186,130 @@ class DrupalClient extends GuzzleHttp\Client {
   public function userIsLoggedIn() {
     $user = $this->app['session']->get('user', FALSE);
     return !empty($user);
+  }
+
+  /**
+   * Returns a flat list of terms for the requested vocabulary.
+   * Response is cached if a cache is available.
+   * @param $vocab_name Vocabulary machine name or Vocabulary ID.
+   * @return mixed
+   */
+  public function getTermsForVocab($vocab_name) {
+
+    // Check object first.
+    if (isset($this->terms[$vocab_name])) {
+      return $this->terms[$vocab_name];
+    }
+
+    // Then memcache.
+    $cache_key = 'vocab/' . $vocab_name;
+    $this->terms[$vocab_name] = $this->cache->get($cache_key);
+
+    if ($this->terms[$vocab_name]) {
+      return $this->terms[$vocab_name];
+    }
+
+    // Finally: Drupal.
+    $result_raw = $this->post('taxonomy_vocabulary/getTree', [
+      'body' => json_encode(array('vid' => $vocab_name)),
+    ]);
+
+    $result = $result_raw->json();
+
+    // Build an array indexed by tid, as it's more useful:
+    $terms = array();
+    foreach ($result as $term) {
+      $terms[$term['tid']] = $term;
+    }
+
+    $this->terms[$vocab_name] = $terms;
+
+    // If we hit Drupal, set the result in memcache.
+    $this->cache->set($cache_key, $this->terms[$vocab_name]);
+
+    return $this->terms[$vocab_name];
+  }
+
+  /**
+   * Returns a list of child terms of a specified parent.
+   * Terms will be sorted by weight.
+   * @param $vocab_name
+   * @param int $parent
+   * @return array
+   */
+  public function getTermsForVocabAtLevel($vocab_name, $parent = 0) {
+
+    $terms = $this->getTermsForVocab($vocab_name);
+
+    $terms_filtered = array();
+
+    foreach ($terms as $term) {
+
+      $this_parent = intval($term['parents'][0]);
+
+      if ($parent == $this_parent) {
+        $terms_filtered[] = $term;
+      }
+    }
+
+    // Sort terms by weight.
+    usort($terms_filtered, function ($a, $b) {
+      if ($a['weight'] == $b['weight']) {
+        return 0;
+      }
+      return ($a['weight'] > $b['weight']) ? 1 : -1;
+    });
+
+    return $terms_filtered;
+  }
+
+  /**
+   * Loads a node by ID.
+   * @param $nid ID of the node to load.
+   * @param bool $cache Whether to use the cache. Defaults to true.
+   */
+  public function getNode($nid, $cache = TRUE) {
+
+    if ($cache) {
+      $cache_key = 'node/' . $nid;
+      $cache_result = $this->cache->get($cache_key);
+
+      if ($cache_result) {
+        return $cache_result;
+      }
+    }
+
+    $node_response = $this->get('entity_node/' . $nid);
+    $node = $node_response->json();
+
+    if ($cache) {
+      $this->cache->set($cache_key, $node);
+    }
+
+    return $node;
+  }
+
+  /**
+   * Saves or updates a node as appropriate.
+   * @param $node
+   */
+  public function saveNode(&$node) {
+
+    if (empty($node['nid'])) {
+      // No nid, create one with a post.
+      $response_raw = $this->post('node.json', [
+        'body' => json_encode($node),
+      ]);
+
+      // Save the nid into the passed node so it can be used.
+      $response = $response_raw->json();
+      $node['nid'] = $response['nid'];
+    }
+    else {
+      // Nid is set, update with a put.
+      $this->put('entity_node/' . $node['nid'], array(
+        'body' => json_encode($node),
+      ));
+    }
   }
 }
